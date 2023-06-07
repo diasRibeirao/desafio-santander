050198bfa7e9c7ab25ff176594d236b71df30305
Emerson de Oliveira <emersondiaspd@gmail.com>
2023-06-07 00:48:43 -0300

    Alterando o insert do migration para ser contas diferentes

--- src/main/resources/db/migration/V2__insert-table-clientes.sql
+++ src/main/resources/db/migration/V2__insert-table-clientes.sql
@@ -5,7 +5,7 @@ INSERT INTO clientes(NOME,PLANO_EXCLUSIVE,SALDO,NUMERO_DA_CONTA,DATA_DE_NASCIMEN
 VALUES('Luana Dias de Oliveira',1,120040,'002',parsedatetime('1989-03-10-00.00.00','yyyy-MM-dd-hh.mm.ss'));
 
 INSERT INTO clientes(NOME,PLANO_EXCLUSIVE,SALDO,NUMERO_DA_CONTA,DATA_DE_NASCIMENTO)
-VALUES('Miguel Dias de Oliveira',1,9000.1,'002',parsedatetime('2014-08-14-00.00.00','yyyy-MM-dd-hh.mm.ss'));
+VALUES('Miguel Dias de Oliveira',1,9000.1,'003',parsedatetime('2014-08-14-00.00.00','yyyy-MM-dd-hh.mm.ss'));
 
 INSERT INTO clientes(NOME,PLANO_EXCLUSIVE,SALDO,NUMERO_DA_CONTA,DATA_DE_NASCIMENTO)
-VALUES('Marcos Teste',0,90000,'002',parsedatetime('2014-08-14-00.00.00','yyyy-MM-dd-hh.mm.ss'));
+VALUES('Marcos Teste',0,90000,'004',parsedatetime('2014-08-14-00.00.00','yyyy-MM-dd-hh.mm.ss'));
